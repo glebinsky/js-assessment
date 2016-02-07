bb31fe74f75f551ff16536c74a29fc72df30865a
Radutsky, Gleb (CORP) <Gleb.Radutsky@adp.com>
2016-02-07 17:18:23 -0500

    Async tests

--- app/async.js
+++ app/async.js
@@ -2,10 +2,25 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function(resolve, reject) {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData : function(url) {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.addEventListener('load', onLoad);
+      xhr.addEventListener('error', function(ev) { reject(); });
+      xhr.open('GET', url);
+      xhr.send();
 
+      /////////////
+
+      function onLoad(ev) {
+        var responseObj = JSON.parse(this.responseText);
+        resolve(_.pluck(responseObj.people, 'name').sort());
+      }
+    });
   }
 };
