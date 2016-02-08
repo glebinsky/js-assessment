de39719ff4e88abac656f9f3ea08fc35d90bb2ca
Radutsky, Gleb (CORP) <Gleb.Radutsky@adp.com>
2016-02-08 14:14:48 -0500

    Flow control (complete) and count (wip)

--- app/count.js
+++ app/count.js
@@ -2,6 +2,16 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var currentCount = start,
+      intervalId = setInterval(function() {
+      if(currentCount > end) {
+        clearInterval(intervalId);
+        return;
+      }
 
+      console.log(currentCount++);
+    }, 100);
+
+    return intervalId;
   }
 };
--- app/flowControl.js
+++ app/flowControl.js
@@ -11,5 +11,26 @@ exports.flowControlAnswers = {
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
 
+    var fizz = 'fizz',
+      buzz = 'buzz',
+      result = '';
+
+    if(num % 3 === 0) {
+      result += fizz;
+    }
+
+    if(num % 5 === 0) {
+      result += buzz;
+    }
+
+    if(result !== '') {
+      return result;
+    }
+
+    if(typeof num === 'number') {
+      return num;
+    } else {
+      return false;
+    }
   }
 };
