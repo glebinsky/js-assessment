6114a965009508050f7b96d9f7e0a54157d2ef62
Radutsky, Gleb (CORP) <Gleb.Radutsky@adp.com>
2016-02-08 19:09:06 -0500

    Logical operators with added tests

--- tests/app/logicalOperators.js
+++ tests/app/logicalOperators.js
@@ -10,6 +10,7 @@ describe('logical operators', function(){
     expect(logicalOperatorsAnswers.or(true, true)).to.be.ok;
     expect(logicalOperatorsAnswers.or(false, false)).not.to.be.ok;
     expect(logicalOperatorsAnswers.or(3, 4)).to.not.eq(7);
+    expect(logicalOperatorsAnswers.or(0, 4)).to.be.ok;
   });
 
   it('you should be able to work with logical and', function() {
@@ -18,5 +19,7 @@ describe('logical operators', function(){
     expect(logicalOperatorsAnswers.and(true, false)).not.to.be.ok;
     expect(logicalOperatorsAnswers.and(true, true)).to.be.ok;
     expect(logicalOperatorsAnswers.and(3, 4)).to.be.ok;
+    expect(logicalOperatorsAnswers.and(3, 0)).not.to.be.ok;
+    expect(logicalOperatorsAnswers.and(-1, 4)).to.be.ok;
   });
 });
